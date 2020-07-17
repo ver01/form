e253cfb8fdb2c396543870c7e128215c864f38c7
ver01Jerry <46354292+ver01Jerry@users.noreply.github.com>
2020-07-17 15:51:44 +0800

    Update unCtrlForm.js

--- src/core/mode/unCtrlForm.js
+++ src/core/mode/unCtrlForm.js
@@ -65,11 +65,13 @@ export default class Form extends Component {
         if (this.props.formProps) {
             const { onValidate } = this.props.formProps;
             if (typeof onValidate === "function") {
-                if (Object.keys(this.rootRuntimeError).length) {
-                    onValidate(this.rootRuntimeError);
-                } else {
-                    onValidate(null);
-                }
+                setTimeout((() => {
+                    if (Object.keys(this.rootRuntimeError).length) {
+                        onValidate(this.rootRuntimeError);
+                    } else {
+                        onValidate(null);
+                    }
+                }).bind(this), 300);
             }
         }
     }
